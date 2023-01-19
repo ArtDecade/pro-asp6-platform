e0f6764a81d65a9e42192e65e538d9f638c3fe7c
William White <billwhite@RCPCONSULTING.BIZ>
2023-01-19 15:24:14 -0500

    Chapter 12, thru p. 311.

---
+++ Platform/Middleware.cs
@@ -0,0 +1,34 @@
+﻿namespace Platform
+{
+    public class QueryStringMiddleware
+    {
+        private RequestDelegate? next;
+
+        public QueryStringMiddleware()
+        {
+            // do nothing
+        }
+
+        public QueryStringMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.Request.Method == HttpMethods.Get 
+                && context.Request.Query["custom"] == "true")
+            {
+                if(!context.Response.HasStarted)
+                {
+                    context.Response.ContentType = "text/plain";
+                }
+                await context.Response.WriteAsync("Class-based Middleware \n");
+            }
+            if (next != null)
+            {
+                await next(context);
+            }
+        }
+    }
+}

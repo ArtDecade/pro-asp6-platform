74392c015cae867dde0f84b0ecf544914596cb66
William White <billwhite@RCPCONSULTING.BIZ>
2023-01-24 19:44:08 -0500

    Thru end of Ch 13

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -2,20 +2,41 @@
 using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("{first:alpha:length(3)}/{second:bool}", async context => {
-    await context.Response.WriteAsync("Request was routed \n");
-    foreach(var kvp in context.Request.RouteValues)
-    {
-        await context.Response
-            .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-    }
+builder.Services.Configure<RouteOptions>(opts =>
+{
+    opts.ConstraintMap.Add("countryName",
+        typeof(CountryRouteConstraint));
 });
 
-app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
-app.MapGet("size/{city?}", Population.Endpoint)
-    .WithMetadata(new RouteNameMetadata("population"));
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    Endpoint? end = context.GetEndpoint();
+    if (end != null)
+    {
+        await context.Response
+        .WriteAsync($"{end.DisplayName} Selected \n");
+    }
+    else
+    {
+        await context.Response.WriteAsync("No endpoint selected\n");
+    }
+    await next();   
+});
+
+app.Map("{number:int}", async context =>
+{
+    await context.Response.WriteAsync("Routed to the int endpoint");
+}).WithDisplayName("Int Endpoint")
+    .Add(b => ((RouteEndpointBuilder)b).Order = 1);
+
+app.Map("{number:double}", async context =>
+{
+    await context.Response.WriteAsync("Routed to the double endpoint.");
+}).WithDisplayName("Double EndPoint")
+    .Add(b => ((RouteEndpointBuilder)b).Order = 2);
 
 app.MapFallback(async context =>
 {
